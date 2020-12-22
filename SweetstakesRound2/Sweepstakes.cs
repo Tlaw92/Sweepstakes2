787d35eed0afac113e9fc92ed898a93cc2a60c7d
Travis Lawson <travislawclimb@gmail.com>
2020-12-21 16:25:31 -0800

    Added logic for Contestant Registration, Choose Winner and Print Contestant info.

--- SweetstakesRound2/Sweepstakes.cs
+++ SweetstakesRound2/Sweepstakes.cs
@@ -10,30 +10,53 @@ namespace SweetstakesRound2
     class Sweepstakes
     {
         //Variables
+        public Contestant bigWinner;
         private Dictionary<int, Contestant> contestants;
         private string name;
-        public string Name { get; set; }
-
+        public string Name { get { return name; } } 
+        
         //Constructor
         public Sweepstakes(string name)
         {
-
+            this.name = name;
+            contestants = new Dictionary<int, Contestant>();
         }
 
         //Methods
-
-        public void RegisterContestant(Contestant Contestant)
+        public int MakeRandomNumber()
         {
+            Random rnd = new Random();
+            int rando = rnd.Next(1000, 1999);
+            return rando;
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+            contestant.RegistrationNumber = MakeRandomNumber();
+            UserInterface.GetUserInputFor(contestant);
+            UserInterface.GiveUserInformation(contestant.RegistrationNumber, contestant.FirstName, name); //or Name?
 
         }
+
+
         public Contestant PickWinner()
         {
-            
+            int winningNumber = MakeRandomNumber();
+            // a random number is drawn
+            // compare against every contestant reg number
+            foreach (KeyValuePair<int, Contestant> winner in contestants)
+            {
+                if (winningNumber == winner.Key) 
+                {
+                    bigWinner = winner.Value;
+                }
+            }
+            return bigWinner;
         }
 
         public void PrintContestantInfo(Contestant contestant)
         {
-
+            UserInterface.PrintContestantInformaton(contestant);
         }
     }
 }
